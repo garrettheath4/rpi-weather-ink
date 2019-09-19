9f5dc77a217bad840299f3066b84085b4f37702b
Garrett Heath Koller <garrettheath4@gmail.com>
2019-09-19 10:10:18 -0400

    ink-display.py now properly centers text and renders unicode characters on e-ink display

--- ink-display.py
+++ ink-display.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import argparse
+import random
 
 from PIL import Image, ImageFont, ImageDraw
 from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
@@ -9,8 +11,9 @@ from inky import InkyPHAT, InkyWHAT
 
 # phat-specific (as opposed to what-specific)
 COLOR = "yellow"
+font_size = 38
 scale_size = 1
-padding = 0
+temp_unit = u"°F"
 
 
 def main():
@@ -24,47 +27,67 @@ def main():
     draw = ImageDraw.Draw(img)
 
     # TODO: Draw background (white?)
+    for y in range(inky_display.height - 4, inky_display.height):
+        for x in range(inky_display.width, inky_display.width):
+            img.putpixel((x, y), inky_display.RED)
 
-    draw_text(str(get_uv()),         inky_display.BLACK, 2, draw, display_size)
-    draw_text(str(get_cloudiness()), inky_display.BLACK, 3, draw, display_size)
-    draw_text(str(get_high_temp()),  inky_display.BLACK, 1, draw, display_size)
-    draw_text(str(get_low_temp()),   inky_display.BLACK, 4, draw, display_size)
+    draw_text(get_uv(),             inky_display.BLACK, 2, img, display_size)
+    draw_text(get_cloudiness(),     inky_display.BLACK, 3, img, display_size)
+    draw_text(get_high_temp_copy(), inky_display.BLACK, 1, img, display_size)
+    draw_text(get_low_temp_copy(),  inky_display.BLACK, 4, img, display_size)
 
     inky_display.set_image(img)
     inky_display.show()
 
 
-def draw_text(text, font_color, quadrant, draw, display_size):
+def draw_text(text, font_color, quadrant, image, display_size):
     # intuitive_font = ImageFont.truetype(Intuitive, int(22 * scale_size))
     # hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(35 * scale_size))
-    hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(16 * scale_size))
+    hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(font_size * scale_size))
+
+    # Ensure baselines line up by always calculating font height including cap
+    # line height and descender line height (i.e. vertically align "ab" as if it
+    # were as high as "Ay")
+    _, font_height = hanken_medium_font.getsize("Ay")
 
     display_width, display_height = display_size
 
-    text_w, text_h = hanken_medium_font.getsize(str(text))
-    text_x = int(((display_width / 2) - text_w) / 2)
+    text_str = text
+    if type(text) not in (unicode, str):
+        text_str = str(text)
+
+    text_w, text_h = hanken_medium_font.getsize(text_str)
+    text_x = int(max((display_width / 2) - text_w, 0) / 2)
     if quadrant in (1, 4):
         text_x += display_width / 2
-    text_y = 0 + padding
+    text_y = int(max((display_height / 2) - font_height, 0) / 2)
     if quadrant in (3, 4):
         text_y += display_height / 2
-    draw.text((text_x, text_y), str(text), font_color, font=hanken_medium_font)
+    ImageDraw.Draw(image).text((text_x, text_y), text_str, font_color, font=hanken_medium_font)
 
 
 def get_uv():
-    return 7
+    return random.randint(3, 9)
 
 
 def get_cloudiness():
-    return "Sunny"
+    return random.choice([u"Sunny", u"Cloudy"])
 
 
 def get_high_temp():
-    return 85
+    return random.randint(80, 90)
+
+
+def get_high_temp_copy():
+    return str(get_high_temp()) + temp_unit
 
 
 def get_low_temp():
-    return 65
+    return random.randint(60, 70)
+
+
+def get_low_temp_copy():
+    return str(get_low_temp()) + temp_unit
 
 
 if __name__ == "__main__":
